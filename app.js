c25d0f1901f78cfad7b8ec9e7fb548c13591f32d
Maks <klimovrychkov123@gmail.com>
2022-07-12 16:00:06 +0300

    24 practic udemy

--- app.js
+++ app.js
@@ -8,9 +8,50 @@ let personalMovieDB = {
     privat: false
 }
 
-let oneFilm = prompt('Один из последних просмотренных фильмов?', ''),
-    howFilm = prompt('На сколько оцените его?', '')
+let oneFilm, howOneFilm
 
-personalMovieDB.movies.oneFilm = howFilm
+for (let i = 1; i <= personalMovieDB.count; i++) {
+    oneFilm = prompt('Один из последних просмотренных фильмов?', '')
+    howOneFilm = prompt('На сколько оцените его?', '')
+    if (oneFilm.length < 50 && oneFilm != null && howOneFilm != null && oneFilm != '' && howOneFilm != '') {
+        personalMovieDB.movies[oneFilm] = howOneFilm
+    } else {
+        i--
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов!')
+} else if (personalMovieDB.count <= 30) {
+    alert('Вы классический зритель!')
+} else if (personalMovieDB.count > 30) {
+    alert('Вы киноман!')
+} else {
+    alert('Произошла ошибка')
+}
+
+let i = 0;
+
+// do {
+//     oneFilm = prompt('Один из последних просмотренных фильмов?', '')
+//     howOneFilm = prompt('На сколько оцените его?', '')
+//     if (oneFilm.length < 50 && oneFilm != null && howOneFilm != null && oneFilm != '' && howOneFilm != '') {
+//         personalMovieDB.movies[oneFilm] = howOneFilm
+//     } else {
+//         i--
+//     }
+//     i++
+// } while (i < personalMovieDB.count)
+
+// while (i < personalMovieDB.count) {
+//     oneFilm = prompt('Один из последних просмотренных фильмов?', '')
+//     howOneFilm = prompt('На сколько оцените его?', '')
+//     if (oneFilm.length < 50 && oneFilm != null && howOneFilm != null && oneFilm != '' && howOneFilm != '') {
+//         personalMovieDB.movies[oneFilm] = howOneFilm
+//     } else {
+//         i--
+//     }
+//     i++
+// }
 
 console.log(personalMovieDB)
